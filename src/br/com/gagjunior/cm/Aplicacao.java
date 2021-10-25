08d7f64ae7f2c03479551aef5d04cc84bcb4fb59
Gilberto Junior <gagjunior@hotmail.com>
2021-10-24 22:49:27 -0300

    Correção

---
+++ src/br/com/gagjunior/cm/Aplicacao.java
@@ -0,0 +1,12 @@
+package br.com.gagjunior.cm;
+
+import br.com.gagjunior.cm.modelo.Tabuleiro;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		Tabuleiro tab = new Tabuleiro(6, 6, 6);
+		System.out.println("Teste");
+	}
+
+}
+
