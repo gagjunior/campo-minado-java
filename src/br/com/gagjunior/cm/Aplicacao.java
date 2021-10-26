c54a03aea92a54316999f662d9d1b445a5511ce2
Gilberto <gilberto.junior@cbbasfaltos.com.br>
2021-10-26 13:07:52 -0300

    Correção de projeto

--- src/br/com/gagjunior/cm/Aplicacao.java
+++ src/br/com/gagjunior/cm/Aplicacao.java
@@ -4,7 +4,7 @@ import br.com.gagjunior.cm.modelo.Tabuleiro;
 
 public class Aplicacao {
 	public static void main(String[] args) {
-		Tabuleiro tab = new Tabuleiro(6, 6, 6);
+		//Tabuleiro tab = new Tabuleiro(6, 6, 6);
 		System.out.println("Teste");
 	}
 
