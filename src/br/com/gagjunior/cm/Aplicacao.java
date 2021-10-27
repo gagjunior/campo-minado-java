f87a72265bfc48bc48269534abeb2c5762d64793
Gilberto Junior <gagjunior@hotmail.com>
2021-10-27 00:17:50 -0300

    Classe tabuleiro

--- src/br/com/gagjunior/cm/Aplicacao.java
+++ src/br/com/gagjunior/cm/Aplicacao.java
@@ -1,20 +1,15 @@
 package br.com.gagjunior.cm;
 
 import br.com.gagjunior.cm.modelo.Tabuleiro;
+import br.com.gagjunior.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Teste1");
-		
 		Tabuleiro tab = new Tabuleiro(6, 6, 6);
+		new TabuleiroConsole(tab);
 		
-		tab.abrir(3, 3);
-		tab.alternarMarcacao(4, 4);
-		tab.alternarMarcacao(4, 5);
-		
-		System.out.println(tab);
 	}
 
 }
---
+++ src/br/com/gagjunior/cm/visao/TabuleiroConsole.java
@@ -0,0 +1,13 @@
+package br.com.gagjunior.cm.visao;
+
+import br.com.gagjunior.cm.modelo.Tabuleiro;
+
+public class TabuleiroConsole {
+	
+	private Tabuleiro tabuleiro;
+	
+	public TabuleiroConsole(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;		
+	}
+
+}
